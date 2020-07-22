05b572a655d5dae173ea1b0782500feff0dc1d59
YOGA <YOGA@DESKTOP-KEIVQ3I>
2020-07-22 22:21:48 +0530

    Completed

---
+++ Interest Calculation/src/com/wipro/bank/acc/RDAccount.java
@@ -0,0 +1,30 @@
+package com.wipro.bank.acc;
+
+public class RDAccount extends Account {
+
+	public RDAccount(int tenure, float principal)
+	{
+		this.tenure=tenure;
+		this.principal=principal;
+	}
+	@Override
+	public float calculateAmountDeposited()
+	{
+		return tenure*principal*12;
+	}
+
+	@Override
+	public float calculateInterest()
+	{
+		float r=rateOfInterest/100;
+		int n=4;
+		float p=0.0f;
+		int tn=tenure*12;
+		for(int i=0;i<tn;i++)
+		{
+			p+=principal*(float)(Math.pow(1+(r/n),n*((n-1)/12.0))-1);
+			
+		}
+		return p;
+	}
+}
